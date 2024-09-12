36686f9d4391825f08dd5899edd55cdfbf8aedfb
Aditya <ady619@gmail.com>
2024-09-12 06:00:55 +0000

    changes from codespace

--- Task1.js
+++ Task1.js
@@ -4,9 +4,6 @@
 // greaterThan10([1,2,3,11,12,13]);
 
 greaterThan10 = (arr) => {
-    // for (let i = 0; i < arr.length; i++) {
-    //     if(arr[i]>10) console.log(arr[i]);
-    // }
     console.log(arr.filter(item => item>10))
 };
 greaterThan10([1,2,3,11,12,13]);
